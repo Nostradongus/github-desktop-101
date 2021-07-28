0b25356090c6da3616cc5d650eb64651626e5be8
Joshue Jadie <joshue_jadie@dlsu.edu.ph>
2021-07-28 23:50:09 +0800

    added test.ino

---
+++ test.ino
@@ -0,0 +1,40 @@
+/*
+ * [SOURCE-FILE LEVEL DOCUMENTATION]
+ * 
+ * File Name: test.ino 
+ * 
+ * Description:
+ * Test Description
+ * 
+ * Author: Test Man 
+ * 
+ * Last Modified: July 26, 2021
+ */
+
+/*
+ * [FUNCTIONAL LEVEL DOCUMENTATION]
+ * 
+ * This function adds the two addends to produce the sum.  
+ * 
+ * @param x : int - the first addend 
+ * @param y : int - the second addend
+ * 
+ * @return the sum of the two addends
+ */
+int add(int x, int y) {
+   // add the two addends and store them to variable sum. 
+   int sum = x+y;
+
+   // return the sum to the main function.
+   return sum;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
